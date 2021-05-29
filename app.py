86a57de6e6f706714b3363ad1b45600081fa4d50
Aadigoel <aadigoel114@gmail.com>
2021-05-29 18:36:12 +0530

    commit 'Added the feature to shuffle songs'

--- app.py
+++ app.py
@@ -2,9 +2,18 @@ import streamlit as st
 import cv2
 import matplotlib.pyplot as plt
 from deepface import DeepFace
-# from deepface import DeepFace
+import spotipy
+
+from spotipy.oauth2 import SpotifyClientCredentials
 import streamlit as st
 import test
+import random
+
+client_id = '8be5f0367b7a4b54b730b33faa7c1b2c'
+client_secret = '0bc78da67fa44fe985c2586aa74d7e1d'
+
+client_credentials_manager = SpotifyClientCredentials(client_id, client_secret)
+sp = spotipy.Spotify(client_credentials_manager =client_credentials_manager)
 
 header  = st.beta_container()
 inp = st.beta_container()
@@ -17,10 +26,10 @@ with header:
 with inp:
     st.title("Taking the face of the user as input")
     faceCascade = cv2.CascadeClassifier(cv2.data.haarcascades + "haarcascade_frontalface_default.xml")
-    page = st.sidebar.selectbox("Input or Predict", ("Input", "Predict"))
+    # page = st.sidebar.selectbox("Input or Predict", ("Input", "Predict"))
 
-    if page == "Input":
-        test.take_input()
+    # if page == "Input":
+    #     test.take_input()
 
 with pred:
     st.title("Prediction")
@@ -31,5 +40,29 @@ with pred:
     predictions = DeepFace.analyze(img)
 
     st.text("Your emotion is {}".format(predictions['dominant_emotion']))  
+
+    if(predictions['dominant_emotion'] != 'happy'):
+
+
+        playlist_id = '37i9dQZF1DX9XIFQuFvzM4'
+
+        def get_track_ids(playlist_id):
+            music_id_list = []
+            playlist = sp.playlist(playlist_id)
+
+            for item in playlist['tracks']['items']:
+                music_track = item['track']
+                music_id_list.append(music_track['id'])
+            return music_id_list
+
+        track_ids = get_track_ids(playlist_id)
+
+        for i in range(5):
+
+            random.shuffle(track_ids)
+
+            my_html = '<iframe src="https://open.spotify.com/embed/track/{}" width="300" height="100" frameborder="0" allowtransparency="true" allow="encrypted-media"></iframe>'.format(track_ids[0])
+
+            st.markdown(my_html, unsafe_allow_html=True)
 
 
