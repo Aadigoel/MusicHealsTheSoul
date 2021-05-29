278d1755be54a68a41894ced41d8b1c1fb274fd1
Aadigoel <aadigoel114@gmail.com>
2021-05-29 16:02:16 +0530

    commit "Got rid of an unnecssary'

--- app.py
+++ app.py
@@ -21,35 +21,6 @@ with inp:
 
     if page == "Input":
         test.take_input()
-#else:
-    #show_explore_page()
-
-
-
-# # 1.creating a video object
-# video = cv2.VideoCapture(0) 
-# # 2. Variable
-# a = 0
-# # 3. While loop
-# while True:
-#     a = a + 1
-#     # 4.Create a frame object
-#     check, frame = video.read()
-#     # Converting to grayscale
-#     # gray = cv2.cvtColor(frame,cv2.COLOR_BGR2GRAY)
-#     # 5.show the frame!
-#     cv2.imshow("Capturing",frame)
-#     # 6.for playing 
-#     key = cv2.waitKey(1)
-#     if key == ord('q'):
-#         break
-# # 7. image saving
-# showPic = cv2.imwrite("photo.jpg",frame)
-# print(showPic)
-# # 8. shutdown the camera
-# video.release()
-# cv2.destroyAllWindows()
-
 
 with pred:
     st.title("Prediction")
